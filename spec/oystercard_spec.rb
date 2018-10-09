03918044f2c916ed11204390ac344883bcd27686
David Best <david_best@ntlworld.com>
2018-10-09 15:35:07 +0100

    completed exercise 7

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,7 @@ describe Oystercard do
     expect(subject).to be_instance_of(Oystercard)
   end
 
-  it 'The card has balnce of zero?' do
+  it 'The card has balance of zero?' do
     expect(subject.balance).to eq 0
   end
 
@@ -17,8 +17,14 @@ describe Oystercard do
       maximum_balance = Oystercard::MAX_BALANCE
       subject.top_up maximum_balance
       expect{ subject.top_up 1 }.to raise_error "Card Limit of #{maximum_balance} Exceeded"
-
     end
+  end
+
+  it { is_expected.to respond_to(:deduct).with(1).argument }
+
+  it 'deducts an amount from the balance' do
+    subject.top_up(20)
+    expect{ subject.deduct(3) }.to change{ subject.balance }.by -3
   end
 
 end
