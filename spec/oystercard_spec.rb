26fb20f256fc9ddec01b3ffe6ed2355d0dc70ead
David Best <david_best@ntlworld.com>
2018-10-09 16:26:02 +0100

    exercise 8 completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,20 +11,33 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
+#    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up the balance' do
-      maximum_balance = Oystercard::MAX_BALANCE
-      subject.top_up maximum_balance
-      expect{ subject.top_up 1 }.to raise_error "Card Limit of #{maximum_balance} Exceeded"
+      subject.top_up(Oystercard::MAX_BALANCE)
+      expect{ subject.top_up 1 }.to raise_error "Card Limit of #{Oystercard::MAX_BALANCE} Exceeded"
     end
   end
 
-  it { is_expected.to respond_to(:deduct).with(1).argument }
+#  it { is_expected.to respond_to(:deduct).with(1).argument }
 
   it 'deducts an amount from the balance' do
-    subject.top_up(20)
+    subject.top_up(Oystercard::MAX_BALANCE)
     expect{ subject.deduct(3) }.to change{ subject.balance }.by -3
+  end
+
+  it 'is initially not in a journey' do
+    expect(subject).not_to be_in_journey
+  end
+
+  it 'can touch in' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it 'can touch out' do
+    subject.touch_out
+    expect(subject).not_to be_in_journey
   end
 
 end
