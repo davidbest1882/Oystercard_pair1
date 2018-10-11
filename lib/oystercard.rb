c2be05c625f5f05aa1f2376a57446200edd7870d
David Best <david_best@ntlworld.com>
2018-10-11 09:23:12 +0100

    half way through exercise 12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,9 @@
 class Oystercard
   MAX_BALANCE = 90
-  MIN_BALANCE = 1
+  MIN_CHARGE = 1
   attr_reader :balance
   attr_reader :entry_station
+  attr_reader :exit_station
 
   def initialize
     @balance = 0
@@ -17,18 +18,24 @@ class Oystercard
     @balance -= fare
   end
 
+  # def in_journey?
+  #   @in_journey
+  # end
+
   def in_journey?
-    @in_journey
+    !!entry_station
   end
 
-  def touch_in(station)
-    raise "Minimum balance of #{Oystercard::MIN_BALANCE} not met" if balance < MIN_BALANCE
-    @in_journey = true
-    @entry_station = station
+  def touch_in(entry_station)
+    raise "Insufficient balance to touch in" if balance < MIN_CHARGE
+#    @in_journey = true
+    @entry_station = entry_station
   end
 
-  def touch_out
-    @in_journey = false
+  def touch_out(exit_station)
+    deduct(MIN_CHARGE)
+    @entry_station = nil
+    @exit_station = exit_station
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,9 @@
 require './lib/oystercard'
 
 describe Oystercard do
-  let(:station) { double :station }
+#  let(:station) { double :station }
+  let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
 
   it "checks for existance of oystercard" do
     expect(subject).to be_instance_of(Oystercard)
@@ -33,28 +35,35 @@ describe Oystercard do
 
   it 'can touch in' do
     subject.top_up(Oystercard::MAX_BALANCE)
-    subject.touch_in(station)
+    subject.touch_in(entry_station)
     expect(subject).to be_in_journey
   end
 
   it 'can touch out' do
-    subject.touch_out
+    subject.touch_out(exit_station)
     expect(subject).not_to be_in_journey
   end
 
   it 'has minimum balance of 1 for a journey' do
 #    context 'balance is less than 1'
-    expect{subject.touch_in(station)}.to raise_error "Minimum balance of #{Oystercard::MIN_BALANCE} not met"
+    expect{subject.touch_in(entry_station)}.to raise_error "Insufficient balance to touch in"
   end
 
   it 'stores the entry station' do
     subject.top_up(20)
-    subject.touch_in(station)
-    expect(subject.entry_station).to eq station
+    subject.touch_in(entry_station)
+    expect(subject.entry_station).to eq entry_station
   end
-  # it 'when I touch in it returns where I travelled from' do
-  #   subject.top_up(5)
-  #   expect(subject.touch_in(station)).to eq "Touched in at #{station}."
+
+  it 'Stores exit station' do
+    subject.top_up(20)
+    subject.touch_in(entry_station)
+    subject.touch_out(exit_station)
+    expect(subject.exit_station).to eq exit_station
+  end
+  # it 'shows an array of journeys' do
+  #
+  #   expect(subject.show_journeys).to eq [ {"touch_in1" => "touch_out1"}, {"touch_in2" => "touch_out2"} ]
   # end
 
 end
